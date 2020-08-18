91a59e3be7c61a3848d9f323ef6d5cef95990737
booon0112 <69611331+booon0112@users.noreply.github.com>
2020-08-18 10:46:04 +0900

    Update ServletLifeCycle.java

--- src/web_study_02/unit02/ServletLifeCycle.java
+++ src/web_study_02/unit02/ServletLifeCycle.java
@@ -25,7 +25,7 @@ public class ServletLifeCycle extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("doGet");
-//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().append("Served at: ").append("gitHub edit);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
